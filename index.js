3255da929ccfbc3b2c16ac78a2be3a7cc4b56447
sspatari <spatari.stanislav@gmail.com>
2020-05-10 20:35:38 +0300

    Add docker, and docker and docker-compose to project

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
-const PORT = 8081;
+const PORT = 3000;
 const app = express();
 
 app.get('/', function (_req, res) {
   res.send('Hello World!');
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`App listening on http://localhost:${PORT}`);
 });
